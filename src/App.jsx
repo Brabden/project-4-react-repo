22ec950df64f7a78d684caabc6c301b83bee52b4
michael-de-wit <michael.b.dewit@gmail.com>
2025-12-03 15:04:51 -0800

    some gift updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import axios from 'axios';
 import './App.css'
 import Navbar from './components/Navbar'
 import PeopleList from './components/PeopleList';
+// import PeopleModal from './components/PeopleModal'
+import GiftModal from './components/GiftModal'
 import GiftList from './components/GiftList'
 const API_URL = "http://127.0.0.1:8000";
 
 const App = () => {
   const [people, setPeople] = useState([]);
   const [gifts, setGifts] = useState([]);
- 
+
   useEffect(() => {
-    const fetchData = async() => {
+    const fetchData = async () => {
       try {
         // Fetching family members
         const peopleResponse = await axios.get(`${API_URL}/api/people`);
@@ -23,17 +25,19 @@ const App = () => {
       } catch (error) {
         console.error("Error fetching data:", error);
       }
-      };
+    };
 
-      fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
   return (
     <>
-    <Navbar />
-    <h1>Family and Gifts</h1>
-    <PeopleList people={people} />
-    <GiftList gifts={gifts} />
+      <Navbar />
+      <h1>Family and Gifts</h1>
+      <PeopleList people={people} />
+      <GiftList gifts={gifts} />
+      {/* <PeopleModal people={people} /> */}
+      <GiftModal gifts={gifts} />
     </>
   );
 };
