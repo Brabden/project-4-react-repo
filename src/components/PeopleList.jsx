865c85b142a35beae8bdf13c14303d2ac3110a59
michael-de-wit <michael.b.dewit@gmail.com>
2025-12-04 15:03:53 -0800

    add family member

--- src/components/PeopleList.jsx
+++ src/components/PeopleList.jsx
@@ -1,20 +1,55 @@
+import { useState } from 'react';
 import PeopleCard from "./PeopleCard";
 
-const PeopleList = ({ people, onUpdatePerson, onDeletePerson }) => {
-if (!people || people.length === 0) {
-    return <p>No people found.</p>
-}
+const PeopleList = ({ people, onAddPerson, onUpdatePerson, onDeletePerson }) => {
+    const [addName, setAddName] = useState('');
+
+    if (!people || people.length === 0) {
+        return <p>No people found.</p>
+    }
+
+    const handleChange = (e) => {
+        setAddName(e.target.value);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log("handleSubmit e", e);
+        console.log("handleSubmit name", addName);
+        
+        if (addName.trim()) {
+            await onAddPerson(addName);
+            setAddName('');
+        }
+    };
+
     return (
-        <div className="people-list">
-            {people.map((person) => (
-                <PeopleCard
-                key={person.id}
-                person={person}
-                onUpdatePerson={onUpdatePerson}
-                onDeletePerson={onDeletePerson}
-                />
-            ))}
-        </div>
+        <>
+            <div className="people-list">
+                {people.map((person) => (
+                    <PeopleCard
+                        key={person.id}
+                        person={person}
+                        onUpdatePerson={onUpdatePerson}
+                        onDeletePerson={onDeletePerson}
+                    />
+                ))}
+            </div>
+            <div>
+
+                <form className="add-person" onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        name="person-to-add"
+                        id="person-to-add"
+                        onChange={handleChange}
+                        value={addName}
+                        placeholder="Add a family member..."
+                    />
+                    <button type="submit">Add</button>
+                </form>
+            </div>
+        </>
     );
 };
 
